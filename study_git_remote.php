11b801f09f7b900af0e32c96de23efdb2d2dbfd2
AlfonsusDiaz <alfonsus.sutjipto@binus.ac.id>
2021-07-16 15:47:07 +0700

    menambahkan list pada study_git_remote.php

--- study_git_remote.php
+++ study_git_remote.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Coba Git Remote pakai file php</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus animi, velit odit rem, dicta minima delectus atque itaque veritatis numquam nobis tenetur labore non! Numquam accusantium magni odio quaerat. Possimus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
